a44311b6ad5e3bafdf62774bab05f50922d321a7
Eashver Elango <eashvere@gmail.com>
2018-07-11 15:13:27 -0700

    Improved Functionality of Predictions

--- webapp/app.py
+++ webapp/app.py
@@ -5,8 +5,8 @@ import os
 import io
 import xml.etree.cElementTree as ET
 from flask import render_template, request, Flask, redirect, url_for, flash, send_from_directory
-from simple_request import predict_result
 from werkzeug.utils import secure_filename
+import requests
 
 UPLOAD_FOLDER = 'var/www/uploads'
 EXTENSIONS = set(['jpg', 'png', 'jpeg'])
@@ -14,14 +14,33 @@ EXTENSIONS = set(['jpg', 'png', 'jpeg'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-#def create_xml(labels, box, flipped)
+PyTorch_REST_API_URL = 'http://127.0.0.1:4000/predict'
+
+def predict_result(image_path):
+    # Initialize image path
+    image = open(image_path, 'rb').read()
+    assert(image is not None)
+    payload = {'image': image}
+    #print(payload)
+
+    # Submit the request.
+    #print(requests.post(PyTorch_REST_API_URL, files=payload)
+    r = requests.post(PyTorch_REST_API_URL, files=payload).json()
+
+    # Ensure the request was successful.
+    if r['success']:
+        # Loop over the predictions and display them.
+        return r['predictions'][0]['labels'], r['predictions'][0]['labels']
+    # Otherwise, the request failed.
+    else:
+        print('Request failed')
 
 def file_allowed(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in EXTENSIONS
 
 @app.route("/")
 def home_screen():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
@@ -39,7 +58,9 @@ def predict():
             filename = secure_filename(file.filename)
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
-            label, box, flipped = predict_result(Image.open(file_path))
+            print(file_path)
+            label, box = predict_result(file_path)
+            return render_template('result.html', label=label, box=box)
 
 
     return redirect('/')
@@ -50,4 +71,4 @@ if __name__ == '__main__':
     app.secret_key = 'lolUWiscool'
     app.config['SESSION_TYPE'] = 'filesystem'
 
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
