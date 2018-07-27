833cc0273e547300db40808a01e6f0d373d30c8e
Eashver Elango <eashvere@gmail.com>
2018-07-27 13:39:50 -0700

    Improved Visualizations

--- webapp/static/main.js
+++ webapp/static/main.js
@@ -1,3 +1,8 @@
-var div = document.getElementById('submit_button');
-div.style.left = '450px';
-div.style.top = '600px'
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
